6f1ea2868c895a55dfff6b652f0e450544dc09a2
Lifespan56 <lifespan56@gmail.com>
2024-10-15 20:24:15 +0900

    [update]SO.sc추가
    SO관련 스크립트, 오브젝트 추가

---
+++ Assets/ChoHyunHo/SOHandler/PlayerStat.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//스텟 정보를 PlayerStatHandler에서 값 초기화 됨
+public class PlayerStat : MonoBehaviour
+{
+    public float MaxHP;
+    public float MoveSpeed;
+    public BulletSO bulletSO;
+}
