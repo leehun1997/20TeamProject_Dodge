36d62110e156fdd910565ac5cd4c25db6b1673f7
Lifespan56 <lifespan56@gmail.com>
2024-10-16 20:30:57 +0900

    Reapply "[update]Animator, AnimationController.cs, 테스트용 프리팹 추가"
    This reverts commit 1f4ed85ac979499511229ce43807144129022c6a.

---
+++ Assets/ChoHyunHo/SOScripts/PlayerAnimationController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : AnimationController
+{
+    private int isMoving = Animator.StringToHash("isMoving");
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        //dodgeController.OnMoveEvent += Move;
+    }
+
+    private void Move()
+    {
+        animator.SetBool(isMoving, true);
+    }
+}
