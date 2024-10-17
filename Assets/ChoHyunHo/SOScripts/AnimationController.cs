6614080e15d5b84e6fb94ee9c3b15899049cc51c
Lifespan56 <lifespan56@gmail.com>
2024-10-17 15:29:43 +0900

    [fix]Animator, TestFrefab, SO폴더링
    asset폴더>TestFrefab내에 현 상황 모든 조립 프리팹 있음
    animator수정 멈췄을때 move>idle
    SO파일 캐릭터별 폴더링

--- Assets/ChoHyunHo/SOScripts/AnimationController.cs
+++ Assets/ChoHyunHo/SOScripts/AnimationController.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     protected Animator animator;
-    //protected DodgeController dodgeController;//최상위 컨트롤러로 받아와 델리게이트에 애니메이션 출력 함수를 구독시켜야함
+    protected DodgeController dodgeController;//최상위 컨트롤러로 받아와 델리게이트에 애니메이션 출력 함수를 구독시켜야함
     //Delegate? Invoke();시 움직이는함수 + 애니메이션바꾸는함수 같이 실행될것임
 
     protected virtual void Awake()
     {
-        animator = GetComponent<Animator>();
-        //dodgeController=GetComponent<DodgeController>();
+        animator = GetComponentInChildren<Animator>();
+        dodgeController = GetComponent<DodgeController>();
     }
 }
