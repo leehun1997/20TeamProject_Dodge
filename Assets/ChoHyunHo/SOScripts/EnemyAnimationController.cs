392b692089227aa1349deb2000b59ed03d0bdd3a
Lifespan56 <lifespan56@gmail.com>
2024-10-18 15:37:36 +0900

    [update]사망시 애니메이션 처리 방식에따른 스크립트추가
    OnDeath구독형태가 아님
    오브젝트 풀을 이용해 사망시 해당위치에 사망애니메이션만 수행하는 객체 생성

--- Assets/ChoHyunHo/SOScripts/EnemyAnimationController.cs
+++ Assets/ChoHyunHo/SOScripts/EnemyAnimationController.cs
@@ -7,8 +7,6 @@ public class EnemyAnimationController : AnimationController
     private int isMoving = Animator.StringToHash("isMoving");
     private int isDead = Animator.StringToHash("isDead");
 
-    [SerializeField] private GameObject DeathAnimationPrefab;
-
     protected override void Awake()
     {
         base.Awake();
@@ -18,7 +16,7 @@ public class EnemyAnimationController : AnimationController
     {
         dodgeController.OnMoveEvent += Move;
         //healthSystem.OnDeath += OnDeath; 사망시 애니메이션 연출 과정에서 구독시킬 필요가 없어졌다
-        //사망시 DestroyOnDeath에서 사망 애니메이션만 보여줄 프리팹을 생성한다
+        //사망시 DestroyOnDeath에서 사망 애니메이션만 수행할 객체를 생성한다
         //사망애니메이션동안 동작하는 모든 스크립트를 끌 필요도 없고 애니메이션 속도가 달라도 대응된다
         //DestroyOnDeath에서는 사망시 바라본 방향을 가져와야 할것이다
     }
@@ -28,6 +26,7 @@ public class EnemyAnimationController : AnimationController
         animator.SetBool(isMoving, v.magnitude > 0.5f);
     }
     
+    //사망시 애니메이션처리는 별도 객체에서 수행한다
     //private void OnDeath()
     
 }
