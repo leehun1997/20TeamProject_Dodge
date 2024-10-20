3a7cdb48ba8b32ffd1a9fa647ab250cddae5b5cc
devH <146154052+0umj@users.noreply.github.com>
2024-10-20 23:01:22 +0900

    feat: 오디오매니저 추가
    오디오 매니저 스크립트를 사용하여 audio clip을 편하게 가져다 사용할 수 있음.

--- Assets/JJY/Scripts/Management/AudioManager.cs
+++ Assets/JJY/Scripts/Management/AudioManager.cs
@@ -1,17 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum SfxType 
+{
+    Select = 0,
+}
+
+public enum BgmType 
+{
+    Start = 0,
+    InGame,
+    End
+}
+
+
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance; 
+    public static AudioManager Instance;
 
+
+    private AudioSource bgmAudioSource;
+    
+    [Header("AudioClip")]
+    [Tooltip("각 Clip을 넣는 순서는 enum SfxType의 값에 맞춰서 넣어야 합니다.")]
+    [SerializeField] private AudioClip[] bgmClip;
+    [SerializeField] private AudioClip[] sfxClip;
+   
+    
+    [Header("AudioSource Pool")]
+    [SerializeField] private List<AudioSource> audioSourcePools;
+    [SerializeField] private  GameObject audioSourcePrefab;   
+    
+    private int audioSourcePoolsSize = 10;
+
+    private SfxType currentSfxType;
+    
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);  
+            DontDestroyOnLoad(gameObject);
         }
 
         else
@@ -19,12 +51,66 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+
+        bgmAudioSource = GetComponent<AudioSource>();
+        CreateAudioSourcePools();
+    }
+
+
+    private void CreateAudioSourcePools()
+    {
+        audioSourcePools = new List<AudioSource>();
+
+        for (int i = 0; i < audioSourcePoolsSize; i++)
+        {
+            GameObject obj = Instantiate(audioSourcePrefab);
+            AudioSource audioSource = obj.GetComponent<AudioSource>();
+
+            if (audioSource != null)
+            {
+                obj.SetActive(false); 
+                audioSourcePools.Add(audioSource); 
+            }
+        }
+    }
+
+    private AudioSource GetSFXAudioSource()
+    {
+        foreach (var audioSource in audioSourcePools)
+        {
+            if (!audioSource.isPlaying)  
+            {
+                audioSource.gameObject.SetActive(true);  
+                return audioSource;
+            }
+        }
+        return null;
     }
     
-    
-    
-    
-    
-    
+    public void PlaySfx(SfxType sfxType)
+    {
+        AudioSource source = GetSFXAudioSource();
+      
+        if (source != null)  
+        {
+            source.clip = sfxClip[(int)sfxType];
+            source.Play();
+        }
+    }
+
+    public void PlayBGM(BgmType bgmType)
+    {
+        // 기존 bgmAudioSource.clip과 같지 않으면
+        if (bgmAudioSource.clip != bgmClip[(int)bgmType])  
+        {
+            bgmAudioSource.Stop();
+            bgmAudioSource.clip = bgmClip[(int)bgmType];
+            bgmAudioSource.Play();
+        }
+    }
+
 
 }
+
+
+
---
+++ Assets/JJY/Scripts/MenuButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    private Button button;
+    
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        button.onClick.AddListener(Select);
+    }
+
+    public void Select()
+    {
+        AudioManager.Instance.PlaySfx(SfxType.Select);
+    }
+    
+}
