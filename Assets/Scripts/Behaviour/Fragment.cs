a0dca0bcfb939e94475fd330f69d55bcb12b66cb
leehun1997 <81948783+leehun1997@users.noreply.github.com>
2024-10-22 17:52:41 +0900

    For Build

--- Assets/Scripts/Behaviour/Fragment.cs
+++ Assets/Scripts/Behaviour/Fragment.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
+using UnityEngine.U2D.Animation;
 using UnityEngine;
 
 public class Fragment : MonoBehaviour
